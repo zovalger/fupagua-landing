7e283524941cc93b338b794c330fba4c4b4d5c74
CesarEMB <cesarmb26@gmail.com>
2022-12-12 23:38:50 -0400

    cambio de logo y nuevo componente(sin terminar)

---
+++ components/Section_info.jsx
@@ -0,0 +1,28 @@
+import Info_style from "../styles/seccion_info.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+
+function Section_info(props) {
+  return (
+
+    <div className={Info_style.section_presentation}>
+      
+      <div className={Info_style.name_fun}>
+        <div className={Info_style.container_img}></div>
+        <div className={Info_style.vector_container}>
+          <div className={Info_style.vector_1}></div>
+        </div>
+        <div className= {Info_style.presentation}>
+          <h2 href="">FUPAGUA</h2>
+          <p>
+            {props.contenido_headerI}
+          </p>
+        </div>
+      </div>
+
+  </div>
+  )
+}
+
+export default Section_info
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
 import NavLanding from "../components/NavLanding";
+import Section_info from "../components/Section_info";
 // import styles from "../styles/NavLanding/NavLanding.module.scss";
+
 
 
 
@@ -12,7 +14,7 @@ export default function Home() {
 				<title>FUPAGUA</title>
 				<meta
 					name="description"
-					content="fundacion de personas autista del estado guarico"
+					content="Fundación de personas autista del estado guárico"
 				/>
 				{/* <link rel="icon" href="/favicon.ico" /> */}
 				<link rel="icon" href="/logo.jpg" />
@@ -23,7 +25,11 @@ export default function Home() {
 			</header>
 
 			<main className={''}>
-				<section>secciones</section>
+				<section>	<Section_info 
+					contenido_headerI = "Hola mundo"
+				/>
+				</section>
+			
 			</main>
 
 			<footer className={''}></footer>
