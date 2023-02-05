20f25e77e5edfb8fb26ae9872d7f5451e04a50f5
CesarEMB <cesarmb26@gmail.com>
2023-02-04 22:41:23 -0400

    infoL

--- components/InfoLanding.jsx
+++ components/InfoLanding.jsx
@@ -1,5 +1,39 @@
-import Image from 'next/image'
+// React/Next
+import Image from 'next/image';
+
+// CSS
+
+import style from '../styles/InfoLanding/Info.module.css'
 
 export default function InfoLanding(){
-    
+    return(
+        <div className={style.container}>
+
+            <h2>Información</h2>
+
+            <div className={style.container__info}>
+
+                    <div className={style.container__img}>
+
+                        <Image 
+                        src={'/IMG/info_1.jpg'}
+                        width={600}
+                        height={600}
+                        alt="FUPAGUA"
+                        loading='lazy'
+                        />
+
+                    </div>
+
+                    <div className={style.container__text}>
+
+                        <p>Somos una fundación sin animo de lucro llamada FUPAGUA, <strong>Fundación de Personas Autistas del Estado Guárico</strong></p>
+
+                    </div>
+
+            </div>
+
+        </div>
+  
+    )
 }
