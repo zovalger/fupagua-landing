14b801826b66ecf5a90427b33216790890ac1914
CesarEMB <cesarmb26@gmail.com>
2023-02-06 21:12:32 -0400

    servicios_temporal

--- components/InfoLanding.jsx
+++ components/InfoLanding.jsx
@@ -3,7 +3,9 @@ import Image from 'next/image';
 
 // CSS
 
-import style from '../styles/InfoLanding/Info.module.css'
+import style from '../styles/InfoLanding/Info.module.css';
+
+import styleService from '../styles/InfoLanding/Services.module.css';
 
 export default function InfoLanding(){
     return(
@@ -72,7 +74,8 @@ export default function InfoLanding(){
 
                     <div className={style.container__img}>
 
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d982.5429019353846!2d-67.37264612954458!3d9.919661771995493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c2aa93824029f4d%3A0x9a77658cc988e299!2sFEDE%20-%20GUARICO%20(Fundaci%C3%B3n%20de%20Edificaciones%20Educativa)!5e0!3m2!1ses!2sve!4v1675642361713!5m2!1ses!2sve" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1691.3044357309557!2d-67.372931!3d9.919639!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c2aa93824029f4d%3A0x9a77658cc988e299!2sFEDE%20-%20GUARICO%20(Fundaci%C3%B3n%20de%20Edificaciones%20Educativa)!5e1!3m2!1ses!2sve!4v1675731124734!5m2!1ses!2sve" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>                    
+                    
                     </div>
 
                     <div className={style.container__text}>
@@ -84,31 +87,69 @@ export default function InfoLanding(){
                     </div>
 
             </div>
+            
+            <div className={styleService.container}>
+                <h2>Servicios</h2>
 
-            <Services />
+                <div className={styleService.container__slide}>
+
+                        <Services 
+                        titulo = "Este es un texto de prueba para medir el tamaño que debe tener el contenedor de texto"
+                        />
+                        <Services 
+                           titulo = "Este es un texto de prueba para medir el tamaño que debe tener el contenedor de texto"
+                           />
+                        <Services 
+                           titulo = "Este es un texto de prueba para medir el tamaño que debe tener el contenedor de texto"
+                           />
+                            <Services 
+                           titulo = "Este es un texto de prueba para medir el tamaño que debe tener el contenedor de texto"
+                           />
+                            <Services 
+                           titulo = "Este es un texto de prueba para medir el tamaño que debe tener el contenedor de texto"
+                           />
+                            <Services 
+                           titulo = "Este es un texto de prueba para medir el tamaño que debe tener el contenedor de texto"
+                           />
+                            <Services 
+                           titulo = "Este es un texto de prueba para medir el tamaño que debe tener el contenedor de texto"
+                           />
+                    
+                    
+            
+
+                </div>
+
+            </div>
+
         </div>
 
-        // Mapa Fupagua
 
-        
+       
   
     )
 }
 
-
+//Servicios
 function Services(props){
     return(
-        <div>
-            <h2>Servicios</h2>
-            <div>
-                <div>
-                    <button>
-                        {/*insert image*/}
-                    </button>
-                    <h2>{props.titulo}</h2>
-                </div>
-            </div>
 
-        </div>
+                <div className={styleService.container__service}>
+
+                    <div className={styleService.container__img}>
+                        <button>
+                            {/*insert image*/ /*cloudynary*/}
+                        </button>
+                    </div>
+
+                    <div className={styleService.container__text}>
+                        <h2>{props.titulo}</h2> {/*De la base de datos*/}
+                    </div>
+
+                </div>
+           
+
     )
 }
+
+//Modal de los servicios
