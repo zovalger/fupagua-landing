b8ae645e2f70bd096524e5f03bd7c88ccb5ce2ac
CesarEMB <cesarmb26@gmail.com>
2023-02-25 22:40:35 -0400

    estilo de los videos

--- layouts/Layout.jsx
+++ layouts/Layout.jsx
@@ -1,9 +1,19 @@
 import NavLanding from "../layouts/NavLanding";
 import FooterLanding from "../layouts/FooterLanding";
+import Head from "next/head";
 
 function Layout({children, title}) {
   return (
+
       <div>
+        	<Head>
+				<title>FUPAGUA</title>
+				<meta
+					name="description"
+					content="fundaciónn de Personas Autista del Estado Guárico"
+				/>
+				<link rel="icon" href="/logo.jpg" />
+			</Head>
           <header>
           <title>{title}</title>
           <NavLanding />
