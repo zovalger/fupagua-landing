7a451be477a09bf12f147fc41439151b229a4fab
zovalger <zovalger@gmail.com>
2023-03-01 22:47:42 -0400

    add: archivos de estilos

--- components/Biblioteca/LibrosRecomendados/RecommendedBooks_section.jsx
+++ components/Biblioteca/LibrosRecomendados/RecommendedBooks_section.jsx
@@ -1,3 +1,5 @@
+import styles from "./RecommendedBooks_section.module.css";
+
 import { useState } from "react";
 import CategoryGroupRecommendedBook from "./CategoryGroupRecommendedBook";
 import CategorySelector from "./CategorySelector";
@@ -6,11 +8,8 @@ import CategorySelector from "./CategorySelector";
 export default function RecommendedBooks_section({ data }) {
 	const [selectedCategory, setSelectedCategory] = useState(data[0]._id);
 
-	const onSelectCategory = (title) => {
-		// seleccionar categoria (poner titulo)
-
-		setSelectedCategory(title);
-	};
+	// seleccionar categoria (poner titulo)
+	const onSelectCategory = (title) => setSelectedCategory(title);
 
 	return (
 		<div
@@ -20,14 +19,16 @@ export default function RecommendedBooks_section({ data }) {
 
 			{/* recorrido para los selectores de categoria */}
 
-			{data.map((category) => (
-				<CategorySelector
-					title={category._id}
-					onSelectCategory={onSelectCategory}
-					selectedCategory={selectedCategory}
-					key={category._id}
-				/>
-			))}
+			<div>
+				{data.map((category) => (
+					<CategorySelector
+						title={category._id}
+						onSelectCategory={onSelectCategory}
+						selectedCategory={selectedCategory}
+						key={category._id}
+					/>
+				))}
+			</div>
 
 			{/* recorrido de todos los grupos de categorias */}
 
